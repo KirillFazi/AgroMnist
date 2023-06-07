d0dcb4cf65705cc25970c27847137d036c7ece46
KirillBiryulin <izyablok@mail.ru>
2023-06-07 19:19:49 +0300

    PC

--- preprocesing.py
+++ preprocesing.py
@@ -1,44 +1,18 @@
-from keras.datasets import mnist
-from keras.utils import np_utils
+import pandas as pd
 
-num_train = 60000
-num_test = 10000
+train_data = pd.read_csv('data/raw/mnist_train.csv')
+test_data = pd.read_csv('data/raw/mnist_test.csv')
 
-height, width, depth = 28, 28, 1
-num_classes = 10
+X_train = train_data.drop('label', axis=1)
+y_train = train_data['label']
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_test = test_data.drop('label', axis=1)
+y_test = test_data['label']
 
-X_train = (X_train
-           .reshape(num_train, height * width)
-           .astype('float32')
-           ) / 255
+X_train.to_csv('data/processed/X_train.csv', index=False)
+y_train.to_csv('data/processed/y_train.csv', index=False)
 
-X_test = (X_test
-          .reshape(num_test, height * width)
-          .astype('float32')
-          ) / 255
+X_test.to_csv('data/processed/X_test.csv', index=False)
+y_test.to_csv('data/processed/y_test.csv', index=False)
 
-Y_train = np_utils.to_categorical(y_train, num_classes)
-Y_test = np_utils.to_categorical(y_test, num_classes)
-
-with open('data/preprocessed/mnist_train.csv', 'w') as f:
-    for i in range(num_train):
-        f.write(','.join([str(x) for x in X_train[i]]))
-        f.write('\n')
-
-with open('data/preprocessed/mnist_test.csv', 'w') as f:
-    for i in range(num_test):
-        f.write(','.join([str(x) for x in X_test[i]]))
-        f.write('\n')
-
-with open('data/preprocessed/mnist_train_label.csv', 'w') as f:
-    for i in range(num_train):
-        f.write(','.join([str(x) for x in Y_train[i]]))
-        f.write('\n')
-
-with open('data/preprocessed/mnist_test_label.csv', 'w') as f:
-    for i in range(num_test):
-        f.write(','.join([str(x) for x in Y_test[i]]))
-        f.write('\n')
 
