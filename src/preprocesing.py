91920f191e4aeea62bd42946a75f4372861636cd
KirillBiryulin <izyablok@mail.ru>
2023-06-07 22:33:10 +0300

    change dvc storage remote from local to google drive

--- src/preprocesing.py
+++ src/preprocesing.py
@@ -9,11 +9,12 @@ y_train = train_data['label']
 X_test = test_data.drop('label', axis=1)
 y_test = test_data['label']
 
-X_train.to_csv('data/processed/X_train.csv', index=False)
-y_train.to_csv('data/processed/y_train.csv', index=False)
+X_train.to_csv('../dagta/processed/X_train.csv', index=False)
+y_train.to_csv('../data/processed/y_train.csv', index=False)
 
-X_test.to_csv('data/processed/X_test.csv', index=False)
-y_test.to_csv('data/processed/y_test.csv', index=False)
+
+X_test.to_csv('../data/processed/X_test.csv', index=False)
+y_test.to_csv('../data/processed/y_test.csv', index=False)
 
 
 """dvc stage add -n prepare \
