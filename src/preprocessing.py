4114677986dad0e73f614dbe654aa18aedcec75a
KirillBiryulin <izyablok@mail.ru>
2023-06-08 10:05:12 +0300

    Create preprocessing pipline

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,23 +1,51 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import sys
+import random
+import os
 
+import yaml
 
-train_data = pd.read_csv('../data/raw/mnist_train.csv')
-test_data = pd.read_csv('../data/raw/mnist_test.csv')
+params = yaml.safe_load(open("params.yaml"))["preprocess"]
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write("\tpython preprocess.py data-file\n")
+    sys.exit(1)
+
+split = params["split"]
+random_seed = params["seed"]
+
+input_dir = os.path.join("data", "raw")
+
+output_dir = os.path.join("data", "processed")
+os.makedirs(output_dir, exist_ok=True)
+
+train_data = pd.read_csv(os.path.join(input_dir, "mnist_train.csv"))
+test_data = pd.read_csv(os.path.join(input_dir, "mnist_test.csv"))
 
 X_train = train_data.drop('label', axis=1)
 y_train = train_data['label']
 
-X_val, X_test, y_val, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=split, random_state=random_seed)
 
 X_test = test_data.drop('label', axis=1)
 y_test = test_data['label']
 
-X_train.to_csv('../dagta/processed/X_train.csv', index=False)
-y_train.to_csv('../data/processed/y_train.csv', index=False)
+X_train.to_csv(os.path.join(output_dir, "X_train.csv"), index=False)
+y_train.to_csv(os.path.join(output_dir, "y_train.csv"), index=False)
 
-X_val.to_csv('../data/processed/X_val.csv', index=False)
-y_val.to_csv('../data/processed/y_val.csv', index=False)
+X_val.to_csv(os.path.join(output_dir, "X_val.csv"), index=False)
+y_val.to_csv(os.path.join(output_dir, "y_val.csv"), index=False)
 
-X_test.to_csv('../data/processed/X_test.csv', index=False)
-y_test.to_csv('../data/processed/y_test.csv', index=False)
+X_test.to_csv(os.path.join(output_dir, "X_test.csv"), index=False)
+y_test.to_csv(os.path.join(output_dir, "y_test.csv"), index=False)
+
+
+"""
+dvc stage add -n prepare \
+                -p prepare.seed,prepare.split \
+                -d src/prepare.py -d data/data.xml \
+                -o data/prepared \
+                python src/prepare.py data/data.xml
+"""
